7df993d43927b3b49cd5f40fe3ae83a91ee88e63
Ruslan <56360676+cruspero@users.noreply.github.com>
2021-08-19 17:27:11 +0500

    add libs

---
+++ libs/html5shiv.js
@@ -0,0 +1,11 @@
+  <!-- Hack for IE6,7,8 to support html5 elements -->
+  <!--[if lt IE 9]>
+    <script>
+      document.createElement('header');
+      document.createElement('nav');
+      document.createElement('main');
+      document.createElement('article');
+      document.createElement('aside');
+      document.createElement('footer');
+    </script>
+    <![endif]-->
